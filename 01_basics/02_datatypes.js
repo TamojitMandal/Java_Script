c2fe0c074085cde26ce28162c99bf4ea364eb622
Tamojit Mandal <tamojitm23@gmail.com>
2024-03-12 11:59:46 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(2+3) //we are using nodejs, not browser
+
+let name = "Tamojit" // string
+let age = 18 // integer
+let isLoggedIn = false // boolean
+
+// null ==> standlone value
+// undefined ==> 
+// symbol
+// object
+
+console.log(typeof name);
