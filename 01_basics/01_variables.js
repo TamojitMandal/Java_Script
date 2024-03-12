46397c6b3808cad378d64d805c9b20a6462cd185
Tamojit Mandal <tamojitm23@gmail.com>
2024-03-12 11:21:20 +0530

    varaibles(let,const)

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 2441139
+let accountEmail = "tamojitm23@gmail.com"
+var accountPassword = "12345"
+accountCity = "Chennai"
+
+// accountId = 2  // not allowed
+
+accountEmail = "mandaltamojit7@gmail.com"
+accountPassword = "7696969"
+accountCity = "Goa"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
