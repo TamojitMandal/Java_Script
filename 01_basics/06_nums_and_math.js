070433e7c29b2422c2f5aaf98e05ce2fe5d60cc7
Tamojit Mandal <tamojitm23@gmail.com>
2024-03-22 18:01:15 +0530

    numbers and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,16 @@
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString());
+
+const otherNumber = 238.89469;
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-In'));
+
+// *********************** Maths ****************************
+
+console.log(Math);
+console.log(Math.abs(-7));
+console.log(Math.round(7.5));
