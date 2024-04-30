f3c6be8152e10c95f58f87790ffa5336e92d7dd8
Tamojit Mandal <tamojitm23@gmail.com>
2024-04-30 19:47:18 +0530

    iterations

---
+++ 05_iterations/one.js
@@ -0,0 +1,60 @@
+// for
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     // console.log(`outer loop value ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`inner loop value ${j} and inner loop ${i}`);
+//         console.log(i + '*' + j + ' = ' + i*j);
+//     }
+    
+// }
+
+// let myArray = ["flash", "batman", "superman"]
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+// }
+
+// break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 7) {
+//         console.log('Detected 7');
+//         break;
+//     }
+//     console.log(`calue of i is ${index}`);
+    
+// }
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 7) {
+//         console.log('Detected 7');
+//         continue;
+//     }
+//     console.log(`calue of i is ${index}`);
+    
+// }
+
+
+
+// while loop
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+// do while loop
+
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
